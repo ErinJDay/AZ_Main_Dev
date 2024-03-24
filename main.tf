ca24102aea814564aad7e9a95d72ac2014f2dc8c
ErinJDay <79655019+ErinJDay@users.noreply.github.com>
2024-03-24 10:36:50 -0700

    Update main.tf adding resourcegroup name

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
 #############################################################################
 
 variable "resource_group_name" {
-  type = string
+  type = "vnet-main"
 }
 
 ### Declare Subsctipion Id Variable in TF CLoud
